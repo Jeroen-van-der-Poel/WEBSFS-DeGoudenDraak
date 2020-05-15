8e14b4d0c7bfb4643fe7a8c18b7fd8a44ff03cc1
Bas van Pelt <49717976+Basje265@users.noreply.github.com>
2020-05-15 13:58:16 +0200

    first pages start

--- DeGoudenDraak/routes/web.php
+++ DeGoudenDraak/routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/menu', 'MenuController@index');
+Route::get('/news', 'NewsController@index');
+Route::get('/contact', 'ContactController@index');
