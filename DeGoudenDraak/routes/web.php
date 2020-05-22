f68267f00ba1b3ba7c6dd4fcd53e6bf11c5ee425
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-05-22 15:37:16 +0200

    customer registration

--- DeGoudenDraak/routes/web.php
+++ DeGoudenDraak/routes/web.php
@@ -16,10 +16,15 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
+//home
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/menu', 'MenuController@index');
 Route::get('/news', 'NewsController@index');
 Route::get('/contact', 'ContactController@index');
 Route::get('/offers', 'OffersController@index');
 Route::get('/option', 'OrderController@index');
+
+//customer
 Route::get('/tablenumber', 'CustomerController@index');
+Route::post('/tablenumber', 'CustomerController@store');
