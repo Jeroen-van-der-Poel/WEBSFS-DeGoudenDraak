5e8bb35ac28c4651bda58b09ddbef7397e0ee6af
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-05-27 13:04:09 +0200

    fix

--- DeGoudenDraak/resources/views/customerorder/index.blade.php
+++ DeGoudenDraak/resources/views/customerorder/index.blade.php
@@ -4,20 +4,20 @@
 
     <div class="container">
         <div class="row d-flex justify-content-between pt-2">
-            <h4>{{ $customer->name }}, tafel: {{ $customer->tablenumber }}</h4>
+            <h4><strong>{{ $customer->name }}, tafel: {{ $customer->tablenumber }}</strong></h4>
             <button class="btn btn-success">Hulp vragen</button>
         </div>
     </div>
     <hr>
     <div class="container">
         <div class="row pt-3">
-            <h1>Bestellen</h1>
+            <h1>Geschiedenis</h1>
         </div>
     </div>
     <hr>
     <div class="container">
         <div class="row pt-3">
-            <h1>Geschiedenis</h1>
+            <h1>Bestellen</h1>
         </div>
     </div>
 @endsection
