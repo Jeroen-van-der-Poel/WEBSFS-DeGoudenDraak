295c09598e093f3a2191c207788abefa58f89d6d
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-05-29 16:26:35 +0200

    fix

--- DeGoudenDraak/public/js/app.js
+++ DeGoudenDraak/public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\DeGoudenDraak\DeGoudenDraak\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\DeGoudenDraak\DeGoudenDraak\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\DeGoudenDraak\WEBSFS-DeGoudenDraak\DeGoudenDraak\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\DeGoudenDraak\WEBSFS-DeGoudenDraak\DeGoudenDraak\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
