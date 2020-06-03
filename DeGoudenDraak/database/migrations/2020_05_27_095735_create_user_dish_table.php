e35fe3fee069f09317df2a2d6c17f4f16b799a15
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-06-03 14:29:20 +0200

    koppeltabel fix

--- DeGoudenDraak/database/migrations/2020_05_27_095735_create_user_dish_table.php
+++ DeGoudenDraak/database/migrations/2020_05_27_095735_create_user_dish_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateUserDishTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('user_dish', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('dish_id');
             $table->integer('amount');
             $table->dateTime('sale_date');
-            $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('dish_id')->references('id')->on('dishes')->onDelete('cascade');
