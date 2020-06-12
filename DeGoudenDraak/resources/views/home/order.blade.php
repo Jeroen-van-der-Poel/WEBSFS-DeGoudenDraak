eb5592ce0b95f29aedae604271de7fade6e696b9
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-06-12 13:21:37 +0200

    button fix

--- DeGoudenDraak/resources/views/home/order.blade.php
+++ DeGoudenDraak/resources/views/home/order.blade.php
@@ -4,20 +4,20 @@
 @section('content')
 
     <div class="row d-flex justify-content-center">
-        <h1>Bestellen als</h1>
+        <h1>Bestellen</h1>
     </div>
         <div class="row d-flex justify-content-center m-5">
             <div class="col-sm-6 d-flex justify-content-center" style="border-right: black 1px solid">
                 @guest
                     <a href="/login">
                         <button class="btn btn-primary" style="height: 100px; width: 200px; max-height: 100px; max-width: 200px;">
-                            Medewerker
+                            Restaurant
                         </button>
                     </a>
                 @else
                     <a href="/cashregister/index">
                         <button class="btn btn-primary" style="height: 100px; width: 200px; max-height: 100px; max-width: 200px;">
-                            Medewerker
+                            Restaurant
                         </button>
                     </a>
                 @endguest
@@ -25,7 +25,7 @@
             <div class="col-sm-6 d-flex justify-content-center">
                 <a href="/homeorder">
                     <button class="btn btn-primary ml-2" style="height: 100px; width: 200px; max-height: 100px; max-width: 200px;">
-                        Klant
+                        Thuisbezorgen
                     </button>
                 </a>
             </div>
