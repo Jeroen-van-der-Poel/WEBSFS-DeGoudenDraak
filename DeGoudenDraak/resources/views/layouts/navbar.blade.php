18bcc4b270297bdc9cb0120cdb933e6f3f134989
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-06-15 14:02:42 +0200

    site titel

--- DeGoudenDraak/resources/views/layouts/navbar.blade.php
+++ DeGoudenDraak/resources/views/layouts/navbar.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>De Gouden Draak</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
