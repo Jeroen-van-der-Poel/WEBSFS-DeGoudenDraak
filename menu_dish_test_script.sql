edfa060885171837765d9dbe340c4565ebc61830
Bas van Pelt <49717976+Basje265@users.noreply.github.com>
2020-06-16 14:51:51 +0200

    test script

---
+++ menu_dish_test_script.sql
@@ -0,0 +1,29 @@
+START TRANSACTION;
+
+-- ADD TEMPORARY COLUMN    
+ALTER TABLE dishes
+ADD COLUMN dishtype_name VARCHAR(45) AFTER dish_category;
+
+-- INSERT MENU ITEMS TO DISHES (CHANGE THE FROM VALUE TO CORRECT DB NAME)
+INSERT INTO dgd_test.dishes (menu_number, menu_addition, name, price, description, dishtype_name)
+SELECT menunummer, menu_toevoeging, naam, price, beschrijving, soortgerecht
+FROM gouden_draak.menu;
+
+-- INSERT DISH CATEGORIES
+INSERT INTO dgd_test.categories (name)
+SELECT DISTINCT soortgerecht
+FROM gouden_draak.menu;
+
+-- INSERT CORRECT CATEGORY ID IN DISH TABLE
+UPDATE dgd_test.dishes as dishes
+SET dish_category =
+(
+    SELECT dishtype.id 
+    FROM dgd_test.categories as dishtype
+    WHERE dishtype.name = dishes.dishtype_name
+)
+WHERE dishes.id IS NOT NULL;
+
+ALTER TABLE dgd_test.dishes DROP dishtype_name;
+
+COMMIT;
